5e7f728b2f6a4f94e28a4d9dd98820c6ff3d1141
ozrush <1+Hr25@go>
2020-06-01 23:14:11 +0300

    HW0 Reformat

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -12,18 +12,5 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.format("Hello TopJava Enterprise!");
-
-        LocalDateTime localDateTime = LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0);
-        System.out.println(localDateTime);
-        LocalTime localTime = LocalTime.of( 10, 0);
-        System.out.println(localTime);
-        localTime = localDateTime.toLocalTime();
-        System.out.println(localTime);
-
-        System.out.println(localTime.compareTo(localDateTime.toLocalTime()) + "eqals" + 0);
-        localTime = LocalTime.of( 13, 0);
-        System.out.println(localTime.compareTo(localDateTime.toLocalTime()) + "biger"+ 1);
-        localTime = LocalTime.of( 5, 0);
-        System.out.println(localTime.compareTo(localDateTime.toLocalTime()) + "lower"+ -1);
     }
 }
